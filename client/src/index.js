fa7137f023b81c703204a95fd0066f7af6617699
David Woolsey <woolsey2316@gmail.com>
2023-06-22 17:19:17 +1000

    user sign in register form

--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import authReducer from "./state"
-import { configureStore } from "@reduxjs/toolkit"
-import { Provider } from "react-redux"
-import { 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
   persistStore,
-  persistsReducer,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER
+  REGISTER,
 } from "redux-persist";
-import storage from "redux-perist/lib/storage";
+import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
-
-const persistsConfig = { key: "root", storage, version: 1}
-const persistedReducer = persistReducer(persistsConfig, authReducer)
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [ FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      }
-    })
-})
-const root = ReactDOM.createRoot(document.getElementById('root'));
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
